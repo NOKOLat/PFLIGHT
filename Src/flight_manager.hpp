d8ff7a5965c2dc8d5ef06c5abcb88754be0168cf
aoi-256 <aoi256jp@Gmail.com>
2025-04-24 21:33:35 +0900

    Add files via upload

---
+++ Src/flight_manager.hpp
@@ -0,0 +1,38 @@
+#ifndef INC_FLIGHT_MANEGER_HPP_
+#define INC_FLIGHT_MANEGER_HPP_
+
+#include <cstdint>
+
+struct FlightManager{
+
+	uint8_t wait = 0;
+	uint8_t loop_count = 0;
+	uint8_t data_index = 0;
+}armloop;
+
+uint8_t IsWaitLoop(){
+
+	return armloop.wait;
+}
+
+void ArmLoopReset(){
+
+	armloop.wait = 0;
+	armloop.loop_count = 0;
+	armloop.data_index = 0;
+}
+
+void ArmLoopSet(){
+
+	armloop.wait = 1;
+}
+
+void ArmLoopClear(){
+
+	armloop.wait = 0;
+	armloop.data_index = 0;
+	armloop.loop_count ++;
+}
+
+
+#endif
